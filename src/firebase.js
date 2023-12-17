f263401add7f32d868e6eed7c6594f9fbbc92df7
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-17 15:35:59 +0330

    make project ready to deploy_4

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,23 @@
+// Importing necessary functions from the Firebase Modular SDK
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-require("dotenv").config();
+
+// Defining Firebase configuration directly (without dotenv)
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
+  apiKey: JSON.stringify(import.meta.env.VITE_APIKEY),
+  authDomain: JSON.stringify(import.meta.env.VITE_AUTHDOMAIN),
+  projectId: JSON.stringify(import.meta.env.VITE_PROJECTID),
+  storageBucket: JSON.stringify(import.meta.env.VITE_STORAGEBUCKET),
+  messagingSenderId: JSON.stringify(import.meta.env.VITE_MESSAGINGSENDERID),
+  appId: JSON.stringify(import.meta.env.VITE_APPID),
 };
 
+// Initializing Firebase with the configuration
 export const app = initializeApp(firebaseConfig);
+
+// Initializing Firebase services (authentication, storage, and Firestore)
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
